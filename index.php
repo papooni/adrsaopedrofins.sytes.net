af8e8423798071207e5c9afe075efc6aac8123f0
papooni <vascosousa@MacBook-Pro-2.home>
2017-06-06 19:51:33 +0100

    width img

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <body>
 
-<img style="align-content: center" src="/img/logo_adr_17.jpg">
+<img style="align-content: center" src="/img/logo_adr_17.jpg" width="50%" height="50%">
 </body>
 </html>
 
